740fe1943b6b0c838d8b289d58648022d244b276
Barsu4ok <KrupovitchDanil@yandex.by>
2023-11-29 21:32:03 +0300

    Creating a personalized service

--- Program.cs
+++ Program.cs
@@ -1,10 +1,21 @@
 using DownloadService;
+using Coravel;
+using Coravel.Scheduling.Schedule;
 
 IHost host = Host.CreateDefaultBuilder(args)
     .ConfigureServices(services =>
     {
-        services.AddHostedService<Worker>();
+        //services.AddHostedService<Worker>();
+        services.AddScheduler();
+        services.AddTransient<ProcessOrder>();
     })
     .Build();
+
+host.Services.UseScheduler(scheduler =>
+{
+    var jobSchedule = scheduler.Schedule<ProcessOrder>();
+    jobSchedule.EverySeconds(5);
+});
+
 
 await host.RunAsync();
